d15f97b0df74b3a6343a73ec2ab89ae18225e8c7
Akshay Narayanan B <90465549+NarayanAkshay11@users.noreply.github.com>
2024-04-03 18:47:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Get all card elements
+  var cards = document.querySelectorAll('.card');
+
+  // Set initial index and total number of cards
+  var currentIndex = 0;
+  var totalCards = cards.length;
+
+  // Define function to move cards
+  function moveCards() {
+    // Hide all cards
+    cards.forEach(function(card) {
+      card.style.display = 'none';
+    });
+
+    // Show current card
+    cards[currentIndex].style.display = 'block';
+
+    // Move to next card or loop back to the first card
+    currentIndex = (currentIndex + 1) % totalCards;
+
+    // Call moveCards function recursively after 3 seconds
+    setTimeout(moveCards, 3000); // Adjust timing here (milliseconds)
+  }
+
+  // Start moving the cards
+  moveCards();
+});
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const burger = document.querySelector('.burger');
     const dropdownMenu = document.querySelector('.dropdown-menu');
